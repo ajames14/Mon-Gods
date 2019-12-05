54e5c6c5ae0498b040cdf11d4fcb6cb200605197
Abi James <a.james1@live.co.uk>
2019-12-05 15:33:35 +0000

    aj-seeds

---
+++ backend/db/seed.js
@@ -0,0 +1,98 @@
+[
+  {
+    spotName: 'Anchor Point',
+    lat: '',
+    long: '',
+    country: 'Morocco',
+    region: 'Taghazout',
+    image: 'https://surfberbere.com/wp-content/uploads/2016/03/anchor_point_surf_berbere_taghazout_holiday_surf_camp_morocco.png', 
+    description: 'A medium to large, long period NW swell is what’s needed to light-up this world-class right, first surfed by Aussies in the 1960’s. From the steep take-off at the outside peak, a seemingly endless succession of speed walls and cutback hooks present themselves. Occasional emerald green rooms appear on the sandy sections down the point. It’s easier to come in at one of the coves and walk back to jump off at the end between sets. Works on all but high tides, unless it’s huge, which is when it may be possible to ride back to Tarhazoute.', 
+    level: 'Advanced', 
+    typeOfWave: 'Point Break',
+    user: ''
+  },
+  {
+    spotName: 'Devils Rock',
+    lat: '',
+    long: '',
+    country: 'Morocco',
+    region: 'Taghazout',
+    image: 'https://surfberbere.com/wp-content/uploads/2016/03/devils_rock_surf_school_morocco.jpg',
+    description: 'Sand bottom beach break with peaky left and rights. Great for improver/beginners, closes out on big swells. To the left, around the headland, is Banana Point and for the experienced surfer a gem lies between the two spots. Can be crowded on the weekends with locals. Relaxed vibe and great cafe on the beach.',
+    level: 'Intermediate', 
+    typeOfWave: 'Beach Break',
+    user: ''
+  },
+  {
+    spotName: 'Boilers',
+    lat: '',
+    long: '',
+    country: 'Morocco',
+    region: 'Central Morocco',
+    image: 'https://surfberbere.com/wp-content/uploads/2016/03/anchor_point_surf_berbere_taghazout_holiday_surf_camp_morocco.png', 
+    description: 'Just south of the lighthouse of Cap Rhir, a washed-up ship boiler gives its name to this consistent and powerful righthander. Vertical lips and speed walls are held up by funnelling trade winds, bouncing off the hills. Experts only as it’s tricky getting in and out because of the urchins and sharp rock shelf. Try the keyhole from behind the large rock to the N. Always bigger than Tarhazoute so it’s often crowded.'
+    level: 'Advanced', 
+    typeOfWave: 'Point Break',
+    user: ''
+  },
+  {
+    spotName: 'Imsouane',
+    lat: '',
+    long: '',
+    country: 'Morocco',
+    region: 'Central Morocco',
+    image: 'https://www.surfholidays.com/accommodation/morocco/imsouane/travel-surf-morocco'
+    description: 'Last stop before Tarhazoute, Immesouane remained authentic until the modern fishing port was built. Fortunately the long, mellow rights keep wrapping into the well-protected south-facing bay, while several well-defined peaks grace the cathedral side. Highly consistent and sometimes crowded.'
+    level: 'Beginners', 
+    typeOfWave: 'Point Break',
+    user: ''
+  },
+  {
+    spotName: 'Killer Point',
+    lat: '',
+    long: '',
+    country: 'Morocco',
+    region: 'Taghazout',
+    image: 'https://surfberbere.com/wp-content/uploads/2016/03/Killer-Point.jpg',
+    description: '‘Killers’ is the most consistent Tarhazoute pointbreak, sucking up all available N-W swells. It’s always bigger than it looks and the paddle-out takes close to 15mins at high tide. In small swells, at low tide, the peak in front of the cliff has a short left (heading straight into some rocky caves) and a sectioning right. As the swell increases, the take-off area moves south and powerful, vertical walls, rumble down the point. Occasional barrels and difficult to make sections. The inside fires at high tide as it’s fully protected from N winds. Handles a lot of size.',
+    level: 'Advanced', 
+    typeOfWave: 'Point Break',
+    user: ''
+  },
+  {
+    spotName: 'Safi',
+    lat: '',
+    long: '',
+    country: 'Morocco',
+    region: 'Central Morocco',
+    image: 'https://theperfectwave.itravelsoftware.com/fotografije_itravel/816/3137_635534102310855326_800_533.jpg',
+    description: 'Safi’s “Garden” remained semi-secret for a long time, with those in the know understandably protective of the long, ultra-fast, throaty barrels that fire down the point north of Safi. Unfortunately, it is very fickle, only breaking in big swells at low tide. There are more rights in the area under similar conditions.',
+    level: 'Advanced', 
+    typeOfWave: 'Point Break',
+    user: ''
+  },
+  {
+    spotName: 'Tamri',
+    lat: '',
+    long: '',
+    country: 'Morocco',
+    region: 'Central Morocco',
+    image: 'https://surfberbere.com/wp-content/uploads/2016/03/tamri_morocco_surf_spot.png',
+    description: 'Located at the mouth of a ‘oued’ irrigating banana plantations, this ultra consistent beachbreak is the go when Tarhazoute is flat. Unfortunately it is onshore in the N winds. Strong rips at 4ft+.',
+    level: 'Advanced', 
+    typeOfWave: 'Reef Break',
+    user: ''
+  },
+  {
+    spotName: 'Banana Point',
+    lat: '',
+    long: '',
+    country: 'Morocco',
+    region: 'Central Morocco',
+    image: 'https://surfberbere.com/wp-content/uploads/2016/03/tamri_morocco_surf_spot.png',
+    description: 'Located at the mouth of a ‘oued’ irrigating banana plantations, this ultra consistent beachbreak is the go when Tarhazoute is flat. Unfortunately it is onshore in the N winds. Strong rips at 4ft+.',
+    level: 'Beginners', 
+    typeOfWave: 'Point Break',
+    user: ''
+  }
+]
