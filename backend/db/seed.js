ff03015a49d1e2bf712e3dc877a6a2eed46a3e99
Awal Yusuf <awaly1992@gmail.com>
2019-12-05 15:40:49 +0000

    Awal-seeds

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -30,7 +30,7 @@
     country: 'Morocco',
     region: 'Central Morocco',
     image: 'https://surfberbere.com/wp-content/uploads/2016/03/anchor_point_surf_berbere_taghazout_holiday_surf_camp_morocco.png', 
-    description: 'Just south of the lighthouse of Cap Rhir, a washed-up ship boiler gives its name to this consistent and powerful righthander. Vertical lips and speed walls are held up by funnelling trade winds, bouncing off the hills. Experts only as it’s tricky getting in and out because of the urchins and sharp rock shelf. Try the keyhole from behind the large rock to the N. Always bigger than Tarhazoute so it’s often crowded.'
+    description: 'Just south of the lighthouse of Cap Rhir, a washed-up ship boiler gives its name to this consistent and powerful righthander. Vertical lips and speed walls are held up by funnelling trade winds, bouncing off the hills. Experts only as it’s tricky getting in and out because of the urchins and sharp rock shelf. Try the keyhole from behind the large rock to the N. Always bigger than Tarhazoute so it’s often crowded.',
     level: 'Advanced', 
     typeOfWave: 'Point Break',
     user: ''
@@ -41,8 +41,8 @@
     long: '',
     country: 'Morocco',
     region: 'Central Morocco',
-    image: 'https://www.surfholidays.com/accommodation/morocco/imsouane/travel-surf-morocco'
-    description: 'Last stop before Tarhazoute, Immesouane remained authentic until the modern fishing port was built. Fortunately the long, mellow rights keep wrapping into the well-protected south-facing bay, while several well-defined peaks grace the cathedral side. Highly consistent and sometimes crowded.'
+    image: 'https://www.surfholidays.com/accommodation/morocco/imsouane/travel-surf-morocco',
+    description: 'Last stop before Tarhazoute, Immesouane remained authentic until the modern fishing port was built. Fortunately the long, mellow rights keep wrapping into the well-protected south-facing bay, while several well-defined peaks grace the cathedral side. Highly consistent and sometimes crowded.',
     level: 'Beginners', 
     typeOfWave: 'Point Break',
     user: ''
@@ -94,5 +94,123 @@
     level: 'Beginners', 
     typeOfWave: 'Point Break',
     user: ''
+  },
+  
+  {
+    spotName: 'Tapia de Casariego',
+    Lat: '43.5702° N',
+    Long: '6.9439° W',
+    country: 'Spain',
+    region: 'Asturias',
+    image: 'https://lasplayas.net/wp-content/uploads/2018/05/playa-penarronda.jpg',
+    description: `Good-quality, consistent beachbreak with semi-permanent, hollow lefthander, fed by a stream. Best mid to low tide, outgoing. Some shelter from NE sea breezes in summer, but it is a year-round wave.
+    Popular surfing beach and contest site that’s often crowded. La Paloma is a small swell spot west of town. Near to town of Tapia de Casariego. Good beach facilities and camping nearby, popular with travelling surfers. Surf shop in nearby town.`,
+    level: ['Beginner'],
+    typeOfWave: 'Reef Break',
+    user: ''
+  },
+  {
+    spotName: 'San Lorenzo',
+    lat: '28.0913° N',
+    long: '16.6574° W',
+    country: 'Spain',
+    region: 'Asturias',
+    image: 'https://gibspain.com/wp-content/uploads/2018/02/Playa-de-San-Lorenzo-Gij%C3%B3n-Asturias-Spain.jpg', 
+    description: `Average-quality beachbreak, picks up a limited amount of swell but surfable during most tides. SW wind OK. Often crowded. This beach is right in the centre of the city of Gijon. Parking is difficult year-round, with meters or expensive underground carparks. Good beach facilities including a surf school,
+                  plus shops and bars at hand and camping fairly nearby. Residential and stormwater pollution.`, 
+    level: ['Intermediate'], 
+    typeOfWave: 'Beach Break',
+    user: ''
+  },
+  {
+    spotName: 'Playa De Berria',
+    lat: '43.4645° N',
+    long: '3.4656° W',
+    country: 'Spain',
+    region: 'Cantabria',
+    image: 'https://turismodecantabria.com/imagenes/Playas/CDBE773B-42AD-53EA-E6A6-4BA521F5638B.jpg/resizeMod/0/1200/imagen.jpg', 
+    description: `Quite long beach with several peaks, depending on sandbars and state of tide. Needs at least a medium swell to work. Moderately consistent.
+    Fairly uncrowded. Quiet spot, practically deserted in winter. Fairly popular with beachgoers in summer. Parking not normally a problem, except during the busiest times. 
+    Good beach facilities. Camping nearby. Slightly polluted.`, 
+    level: ['Beginner'], 
+    typeOfWave: 'Beach Break',
+    user: ''
+  },
+  {
+    spotName: 'Playa De Cuberris',
+    lat: '43.4962° N',
+    long: '3.6151° W',
+    country: 'Spain',
+    region: 'Cantabria',
+    image: 'https://live.staticflickr.com/4084/4963802369_cddf286de1_b.jpg', 
+    description: '250m long sand beach in a beautiful bay. Big parking, Cafés and Restaurants close by. Venue for many Beach volleyball tournaments and my preferred sport.', 
+    level: ['Intermediate'], 
+    typeOfWave: 'Beach Break',
+    user: ''
+  },
+  {
+    spotName: 'Pantin',
+    lat: '43.6373° N',
+    long: '8.1200° W',
+    country: 'Spain',
+    region: 'Galicia',
+    image: 'http://www.pantinsurfcamp.com/imagenes/img2Localizacion.jpg', 
+    description: `The most popular surf spot in Galicia, home to an annual pro contest. Ultra consistent, Pantin hoovers up W-NW swell and can hold quite a bit of size. Both left and right peaks can line up and spitting barrels are common. 
+    Better low to mid but surfable on all tides. Strong rips with paddling channel at northern end. Can get crowded, with some localism. Fills up in summer with beachgoers and travelling surfers. Good beach facilities and a carpark freecamp scene. Clean water.`, 
+    level: ['Intermediate'], 
+    typeOfWave: 'Beach Break',
+    user: ''
+  },
+  {
+    spotName: 'Patos',
+    lat: '43.6373° N',
+    long: '8.1200° W',
+    country: 'Spain',
+    region: 'Galicia',
+    image: 'http://www.turismo.gal/imaxes/mdaw/mduw/~edisp/~extract/TURGA050534~1~staticrendition/tg_carrusel_cabecera_grande.jpg', 
+    description: `Good quality left and righthand reefbreak. Fairly short rides with some barrel sections. Needs large NW swell and mid to high tide to get going. SW wind OK.
+    Near to the city of Vigo, so always crowded when on, with a degree of localism. Surf shop nearby. Some residential and stormwater pollution.`, 
+    level: ['Beginners'], 
+    typeOfWave: 'Beach Break',
+    user: ''
+  },
+  {
+    spotName: 'Torrevieja La Mata',
+    lat: '38.0186° N',
+    long: '0.6530° W',
+    country: 'Spain',
+    region: 'Mediterranean',
+    image: 'https://media-cdn.tripadvisor.com/media/photo-s/03/ac/8b/ed/la-mata-beach.jpg', 
+    description: 'Long open, east-facing beach north of Torrevieja. Surfers concentrate in the south where some rocks help to stabalise the sand. Mostly rights. Medium consistency and sometimes crowded.', 
+    level: ['Intermediate'], 
+    typeOfWave: 'Beach Break',
+    user: ''
+  },
+  {
+    spotName: 'Playa de Mundaka',
+    lat: '43.4047° N',
+    long: '2.6993° W',
+    country: 'Spain',
+    region: 'Pais Vasco',
+    image: 'https://shipitwise.com/wp-content/uploads/2016/09/Mundaka-Spain.jpg', 
+    description: `Mundaka is a dream lefthand barrel and possibly the best rivermouth wave in the world. A long triangular sandbank catches the stronger NW swells, creating a long flawless tube with rides of up to 150-200 meters possible.
+    Always crowded. Moderately dangerous, depending on swell size, wind and crowd. Be careful of the rip. Fairly consistent although almost never works in summer and only works from low to about mid tide.`, 
+    level: ['Advanced'], 
+    typeOfWave: 'Beach Break',
+    user: ''
+  },
+  {
+    spotName: 'Sopelana',
+    lat: '43.4047° N',
+    long: '2.6993° W',
+    country: 'Spain',
+    region: 'Pais Vasco',
+    image: 'https://photos.eltiempo.es/29/61/bac823c6adda67f8b2882b9d580d65e1_720x560.jpg', 
+    description: `Very consistent beachbreak with several peaks. Good semi-permanent high-tide peak at western end of beach. Works on small to medium swells. 
+                One of the most crowded spots in Spain. Highly competitive. Surf shops and camping nearby. Estuarine and stormwater pollution, particularly after heavy rains.`, 
+    level: ['Beginners'], 
+    typeOfWave: 'Beach Break',
+    user: ''
   }
+  
 ]
