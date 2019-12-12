185fa8ae0a19a375977f49ae59c6951d44a1ef08
Abi James <a.james1@live.co.uk>
2019-12-12 19:05:29 +0000

    styling changes country page

--- frontend/src/components/Country.js
+++ frontend/src/components/Country.js
@@ -14,7 +14,7 @@ const CountrySpots = (props) => {
       .then(resp => resp.json())
       .then(resp => {
         setData(resp),
-        getCountry(resp)
+          getCountry(resp)
 
       })
     return () => console.log('Unmounting component')
@@ -54,7 +54,7 @@ const CountrySpots = (props) => {
           {/* <form className="form" onClick={(e) => this.handleFilter(e)}> */}
           <div id='dropdowns'>
             <div className="field">
-              <div className="control">
+              <div className="control filters">
                 <label className="label">Select your level:</label>
                 <div className="select is-small">
                   <select onChange={(e) => levelFilter(e)}>
@@ -68,7 +68,7 @@ const CountrySpots = (props) => {
               </div>
             </div>
             <div className="field">
-              <div className="control">
+              <div className="control filters">
                 <label className="label">User Rating:</label>
                 <div className="select is-small">
                   <select onChange={(e) => ratingFilter(e)}>
@@ -82,56 +82,63 @@ const CountrySpots = (props) => {
                 </div>
               </div>
             </div>
+
+            <div className="control filters">
+              <label className="label">Search Spots:</label>
+              <input
+                className="input is-hovered is-small"
+                type="text"
+                id='search-input'
+                placeholder='Search...'
+                name='query'
+                onChange={formFilter.bind(this)}
+              />
+            </div>
           </div>
-          <input
-            className="input"
-            type="text"
-            id='search-input'
-            placeholder='Search...'
-            name='query'
-            onChange={formFilter.bind(this)}
-          />
         </form>
-        <div className="container">
-          <div className="columns is-mobile is-multiline">
-            {countries
-              .filter(elem => {
-                if (filterType === "All") {
-                  return elem
-                } else return elem.level === filterType || elem.level === 'All levels'  // ===========> some spots are suitible for all levels
-              })
-
-              .filter(elem => {
-                let average
-                if (filterRating === "All") {                            // ===============>  THIS IS TO FILTER OUT THE RATING
-                  return elem
-                } else {
-                  if (elem.rating.length === 0) {
-                    average = 0
-                  } else {
-                    average = (elem.rating.reduce((previous, current) => previous.rate + current.rate)) / elem.rating.length
-                    console.log('average1111', average)
-                  }
-                  console.log('aveage222222', average)
-                  // let average = (elem.rating.rate.reduce((previous, current) => current += previous)/elem.rating.rate.length)
-                  if (average > Number(filterRating) && average <= Number(filterRating) + 1) {
-                    // console.log('filterrrrrrrr RAAAATINGGGGGG', filterRating)
-                    // console.log('++++++1', Number(filterRating) + 1)
-                    // console.log('1-------', Number(filterRating) - 1)
+        <br />
+        <div id="spots-overview">
+          <div className="container">
+            <div className="columns is-mobile is-multiline">
+              {countries
+                .filter(elem => {
+                  if (filterType === "All") {
                     return elem
-                  }
-                }
-              })
+                  } else return elem.level === filterType || elem.level === 'All levels'  // ===========> some spots are suitible for all levels
+                })
 
-              .filter(elem => {
-                // if(this.state.filterType){
-                // return elem.type === this.state.filterType
-                return elem.spotName.toLowerCase().includes(filter.toLowerCase())
-                // || elem.region.toLowerCase().includes(filter.toLowerCase())      =>>>>>>>>> allows for form to also filter field also include spot's region
-              })
-              .map((spot, id) => {
-                return <SpotCard key={id} spot={spot} />                 //   =>>>>>>>>> ADD message for no results found
-              })}
+                .filter(elem => {
+                  let average
+                  if (filterRating === "All") {                            // ===============>  THIS IS TO FILTER OUT THE RATING
+                    return elem
+                  } else {
+                    if (elem.rating.length === 0) {
+                      average = 0
+                    } else {
+                      average = (elem.rating.reduce((previous, current) => previous.rate + current.rate)) / elem.rating.length
+                      console.log('average1111', average)
+                    }
+                    console.log('aveage222222', average)
+                    // let average = (elem.rating.rate.reduce((previous, current) => current += previous)/elem.rating.rate.length)
+                    if (average > Number(filterRating) && average <= Number(filterRating) + 1) {
+                      // console.log('filterrrrrrrr RAAAATINGGGGGG', filterRating)
+                      // console.log('++++++1', Number(filterRating) + 1)
+                      // console.log('1-------', Number(filterRating) - 1)
+                      return elem
+                    }
+                  }
+                })
+
+                .filter(elem => {
+                  // if(this.state.filterType){
+                  // return elem.type === this.state.filterType
+                  return elem.spotName.toLowerCase().includes(filter.toLowerCase())
+                  // || elem.region.toLowerCase().includes(filter.toLowerCase())      =>>>>>>>>> allows for form to also filter field also include spot's region
+                })
+                .map((spot, id) => {
+                  return <SpotCard key={id} spot={spot} />                 //   =>>>>>>>>> ADD message for no results found
+                })}
+            </div>
           </div>
         </div>
       </section>
